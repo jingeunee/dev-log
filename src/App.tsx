b4478e7e988f7f2c2e464367407fa9a9e3fcf797
jink7277 <jink7277@gmail.com>
2022-04-20 21:34:40 +0900

    add router basename

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         <ToastContainer />
 
         <GlobalWrap>
-          <BrowserRouter>
+          <BrowserRouter basename={process.env.PUBLIC_URL}>
             <HeaderWrapper>
               <Header />
             </HeaderWrapper>
